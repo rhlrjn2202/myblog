1a525f3ba0644c674259d53f4049a0868ac5d98f
rhlrjn2202 <rhlrjn2202@gmail.com>
2022-07-27 23:15:25 +0530

    cms added updated

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -4,8 +4,6 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('./src/assets');
 	eleventyConfig.addPassthroughCopy('./src/admin');
 
-
-
 	return {
 		dir: {
 			input: "src",
