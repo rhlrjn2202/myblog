d7277d9452e3df78b50d820775ba6e9a6947058e
rhlrjn2202@gmail.com <rhlrjn2202@gmail.com>
2024-06-19 17:51:21 +0530

    4

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,23 @@
+const sitemap = require("@quasibit/eleventy-plugin-sitemap");
+
 module.exports = function(eleventyConfig) {
 
-	eleventyConfig.addPassthroughCopy('./src/style.css');
-	eleventyConfig.addPassthroughCopy('./src/assets');
-	eleventyConfig.addPassthroughCopy('./src/admin');
+    // Pass-through copy for static files
+    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/admin');
 
+    // Add the sitemap plugin
+    eleventyConfig.addPlugin(sitemap, {
+        sitemap: {
+            hostname: "https://www.yourwebsite.com", // Replace with your website URL
+        },
+    });
 
-
-
-	return {
-		dir: {
-			input: "src",
-			output: "public"
-		}
-	};
-}
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    };
+};
