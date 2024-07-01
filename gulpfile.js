d03454e0e2683245bee74cc1bcfa003cc474a04f
rhlrjn2202@gmail.com <rhlrjn2202@gmail.com>
2024-07-01 12:25:31 +0530

    err resctify

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,60 @@
 const gulp = require('gulp');
-const gzip = require('gulp-gzip');
+const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 const terser = require('gulp-terser');
-const cleanCSS = require('gulp-clean-css');
+const cleanCss = require('gulp-clean-css');
 const critical = require('critical').stream;
-
-async function loadImagemin() {
-    const imagemin = await import('gulp-imagemin');
-    return imagemin.default;
-}
+const gzip = require('gulp-gzip');
 
 // Task to optimize images
-async function optimizeImages() {
-    const imagemin = await loadImagemin();
+gulp.task('optimizeImages', () => {
     return gulp.src('./src/assets/images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./public/assets/images'));
-}
-
-// Task to create gzip files
-function createGzip() {
-    return gulp.src('./public/**/*.{html,css,js}')
-        .pipe(gzip())
-        .pipe(gulp.dest('./public'));
-}
-
-// Task to inline critical CSS
-function inlineCriticalCss() {
-    return gulp.src('public/*.html')
-        .pipe(critical({ base: 'public/', inline: true, minify: true }))
-        .pipe(gulp.dest('public'));
-}
+});
 
 // Task to minify HTML
-function minifyHtml() {
+gulp.task('minifyHtml', () => {
     return gulp.src('./public/**/*.html')
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest('./public'));
-}
+});
 
-// Task to minify JS
-function minifyJs() {
+// Task to minify JavaScript
+gulp.task('minifyJs', () => {
     return gulp.src('./public/**/*.js')
         .pipe(terser())
         .pipe(gulp.dest('./public'));
-}
+});
 
 // Task to minify CSS
-function minifyCss() {
+gulp.task('minifyCss', () => {
     return gulp.src('./public/**/*.css')
-        .pipe(cleanCSS())
+        .pipe(cleanCss())
         .pipe(gulp.dest('./public'));
-}
+});
 
-// Task to build the site
-const build = gulp.series(
-    optimizeImages,
-    inlineCriticalCss,
-    minifyHtml,
-    minifyJs,
-    minifyCss,
-    createGzip
-);
+// Task to generate critical CSS
+gulp.task('criticalCss', () => {
+    return gulp.src('./public/**/*.html')
+        .pipe(critical({ base: 'public/', inline: true, css: ['./public/**/*.css'] }))
+        .on('error', err => console.error(err.message))
+        .pipe(gulp.dest('./public'));
+});
 
-exports.default = build;
-exports.build = build;
-exports.optimizeImages = optimizeImages;
-exports.createGzip = createGzip;
-exports.inlineCriticalCss = inlineCriticalCss;
-exports.minifyHtml = minifyHtml;
-exports.minifyJs = minifyJs;
-exports.minifyCss = minifyCss;
+// Task to create gzip files
+gulp.task('createGzip', () => {
+    return gulp.src('./public/**/*.{html,css,js}')
+        .pipe(gzip())
+        .pipe(gulp.dest('./public'));
+});
+
+// Default task
+gulp.task('build', gulp.series(
+    'optimizeImages',
+    'minifyHtml',
+    'minifyJs',
+    'minifyCss',
+    'criticalCss',
+    'createGzip'
+));
